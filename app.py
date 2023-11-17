2f2d2f384e4fb08d53b7fa561618e9ee5297b1e1
Emad <101589440+e-Ash1@users.noreply.github.com>
2023-11-17 11:55:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey 
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'
+app.secret_key = '********'
 
 # Instantiate Root Directory:
 @app.route('/', methods=['GET', 'POST'])
